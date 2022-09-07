c51df9a0fd1a5338ce7285f80065c22ff434dfbc
Алексей Александров <Alex37v2@gmail.com>
2022-09-08 00:37:09 +1000

    initial commit

---
+++ src/main/java/ru/netology/homework/Radio.java
@@ -0,0 +1,4 @@
+package ru.netology.homework;
+
+public class Radio {
+}
---
+++ src/test/java/ru/netology/homework/RadioTest.java
@@ -0,0 +1,7 @@
+package ru.netology.homework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+}
